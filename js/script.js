0814efb6d3da9936952e5f672780efe8c188f933
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2023-10-10 16:22:34 +0200

    working on javascript

--- js/script.js
+++ js/script.js
@@ -2,26 +2,19 @@ myDiv = document.getElementById('my-text');
 /* myDiv.innerHTML = prezzoFinale */
 
 let chilometri = parseInt(prompt('Quanti chilometri dovrai percorrere?'));
-/* console.log(chilometri); */
 let age = parseInt(prompt('Quanti anni hai?'));
-/* console.log(age); */
 const tariffa = 0.21;
-let prezzoIntero = tariffa * chilometri;
-/* console.log(prezzoIntero); */
-let sconto = 0;
+let price = tariffa * chilometri;
+let scontoUnder18 = price * 0.2;
+let scontoOver65= price * 0.4;
 
 if (isNaN(chilometri) || isNaN(age)){
     myDiv.innerHTML = ('Inserisci correttamente i dati')
     window.location.reload()
 };
 if (age >= 65){
-    let sconto = (prezzoIntero * 0.4);
-    let prezzoFinale= prezzoIntero - sconto;
-    myDiv.innerHTML= ('Il costo del suo biglietto è ' + prezzoFinale.toFixed(2) + '€')
+    price= price - scontoOver65;
 } else if (age < 18){
-    let sconto = (prezzoIntero * 0.2);
-    let prezzoFinale= prezzoIntero - sconto;
-    myDiv.innerHTML= ('Il costo del suo biglietto è ' + prezzoFinale.toFixed(2) + '€')
-} else {
-    myDiv.innerHTML= ('Il costo del suo biglietto è ' + prezzoIntero.toFixed(2) + '€')
-};
+    price= price - scontoUnder18;
+}
+myDiv.innerHTML= ('Il costo del suo biglietto è ' + price.toFixed(2) + '€')
